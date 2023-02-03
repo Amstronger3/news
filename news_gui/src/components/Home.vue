268cee2aff5ee43361a059528d7862d728975720
Aleksandr <Amstronger3@gmail.com>
2023-02-03 16:32:16 +0100

    add gui

---
+++ news_gui/src/components/Home.vue
@@ -0,0 +1,39 @@
+<template>
+    <news-list :news="news">
+
+    </news-list>
+</template>
+
+<script>
+import NewsList from '@/components/NewsList.vue'
+import axios from 'axios';
+
+export default {
+    name: 'Home',
+    components: {
+        NewsList
+    },
+    
+    data() {
+        return {
+            news: ''
+        }
+    },
+    methods: {
+        getResponse(){
+            const path = 'http://0.0.0.0:5000/'
+            axios.get(path)
+            .then((res) => {
+                console.log(res.data)
+                this.news = res.data;
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+            },
+        },
+        created() {
+            this.getResponse();
+        }
+    }
+</script>
---
+++ news_gui/src/components/NewsList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h3>
+            News
+        </h3>
+        <div v-for="new_item in news" :key="new_item.id">
+            <a :href="new_item.source_link"
+            target="_blank">{{ new_item.header_original }}</a>
+        </div>
+    </div>
+</template>
+<script>
+
+export default {
+    props: {
+        news: {
+          type: Array,
+          required: true,
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+</style>
